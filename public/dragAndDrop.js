49e552b0de83a9e3f81bfe02870a3c763728ad21
Guilherme <guirque@gmail.com>
2023-12-19 15:57:04 -0300

    Ajustando Drag And Drop
    Agora, tarefas podem ser movidas apenas para linhas de prioridade (como esperado). Além disso, as tarefas (atividades) mudam de cor e têm sua prioridade atualizada de acordo com a posição em que foram deixadas.

--- public/dragAndDrop.js
+++ public/dragAndDrop.js
@@ -2,6 +2,7 @@ let draggedItem = null;
 
 document.addEventListener('dragstart', function(event) {
    draggedItem = event.target;
+   draggedItem.classList.add('dragged');
    setTimeout(function() {
        draggedItem.style.display = 'none';
    }, 0);
@@ -24,6 +25,35 @@ document.addEventListener('dragleave', function(event) {
 });
 
 document.addEventListener('drop', function(event) {
-   event.target.classList.remove('drag-over');
-   event.target.appendChild(draggedItem);
+
+    let arrastado = document.querySelector('.dragged');
+    console.log(arrastado);
+
+    //Se o espaço for uma linha de prioridade
+    if(event.target.classList.value.match(/prioridade/))
+    {
+        //Identifique a prioridade e a coluna
+        let strColuna = event.target.parentElement.parentElement.id;
+        let titulo = arrastado.querySelector('.tituloAtividade');
+        let prioridade = event.target.classList.value.match(/alta|media|baixa/i)[0];
+        
+        //Modifique a tarefa       
+        switch(strColuna)
+        {
+            case "afazer":
+                titulo.style.backgroundColor = '#E53324';
+                break;
+            case "fazendo":
+                titulo.style.backgroundColor = '#FBE23E';
+                break;
+            case "finalizado":
+                titulo.style.backgroundColor = '#2F993D';
+        }
+        arrastado.querySelector('.nome-prioridade').textContent = prioridade;
+
+        //Coloque a tarefa
+        event.target.classList.remove('drag-over');
+        event.target.appendChild(draggedItem);
+        arrastado.classList.remove('dragged');
+    }
 });
